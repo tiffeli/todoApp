27d3e4d2590bdad584ca65593167ceb4de4d4e8b
Tiffany <tet226@nyu.edu>
2015-12-22 23:57:25 -0800

    add delete, no working

--- toDo.js
+++ toDo.js
@@ -1,10 +1,14 @@
 function addListItem(){
   var text = $("#new-todo").val();
-  var list = $("#todo-list").append("<li id=eachitem>"+text+"</li>");
+  var list = $("#todo-list").append('<li id=eachitem>'+text+' <button class="delete"> Delete </button></li>');
   $("#new-todo").val(' ');
 }
+function deleteItem() {
+ $(this).remove();
+}
+
 $(function() {
     $("#btn-semi-transparent").on("click", addListItem);
-
+    $(".delete").on("click", deleteItem);
 });
 
