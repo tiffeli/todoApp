7bbfe5b944ff64624523538e0bda68ec85d9d8e3
Tiffany <tet226@nyu.edu>
2015-12-23 12:35:52 -0800

    update delete and checkbox, buggy

--- toDo.js
+++ toDo.js
@@ -1,14 +1,32 @@
 function addListItem(){
   var text = $("#new-todo").val();
-  var list = $("#todo-list").append('<li id=eachitem>'+text+' <button class="delete"> Delete </button></li>');
+  var list = $("#todo-list").append("<li><input type='checkbox' class='done' />" +
+    text+" <button class='delete'> Delete </button></li>");
   $("#new-todo").val(' ');
 }
 function deleteItem() {
- $(this).remove();
+ $(this).parent().remove();
+}
+function checkOff() {
+  if( $(this).parent().css('textDecoration') === 'line-through'){
+      $(this).parent().css('textDecoration', 'none');
+  } else {
+  $(this).parent().css('textDecoration', 'line-through');
+  }
 }
 
-$(function() {
+$(document).ready(function() {
     $("#btn-semi-transparent").on("click", addListItem);
-    $(".delete").on("click", deleteItem);
+
+    $(".delete").on("click",deleteItem);
+    $(".done").on("click", checkOff);
 });
+// function handle(e){
+    //     if(e.keyCode === 13){
+    //         alert("Enter was pressed was presses");
+    //     }
+
+    //     return false;
+    // }
+    //how to get enter word detection to add
 
